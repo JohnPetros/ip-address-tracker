83242ea9a5d270c177ba48a4532093a814893b99
JohnPetros <joaopedro.nc@outlook.com>
2024-06-08 22:23:41 -0300

    🖥️ views: set home page allowed methods

--- src/app/views/__init__.py
+++ src/app/views/__init__.py
@@ -4,8 +4,7 @@ from .home_page_view import home_page_view
 
 views = Blueprint("view", __name__)
 
-views.add_url_rule("/", view_func=home_page_view)
-# views.add_url_rule("/details", view_func=details_page_view)
+views.add_url_rule("/", view_func=home_page_view, methods=["GET", "POST"])
 
 
 def init_views(app: Flask):
