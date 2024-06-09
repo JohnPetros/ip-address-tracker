058ec8a9e064479af3250beff9cf98cd1acb2fa7
JohnPetros <joaopedro.nc@outlook.com>
2024-06-08 22:22:55 -0300

    🖥️ views: receive ip address from ui

--- src/app/views/home_page_view.py
+++ src/app/views/home_page_view.py
@@ -1,24 +1,33 @@
-from flask import render_template
+from flask import request, render_template, flash, redirect
 
 from controllers import home_page_controller
 
 
 def home_page_view():
-    data = home_page_controller.execute()
+    try:
+        ip_address = ""
 
-    ip_address = data["ip_address"]
-    address = data["address"]
-    time = data["time"]
-    isp = data["isp"]
-    latitude = data["latitude"]
-    longitue = data["longitue"]
+        if request.method == "POST":
+            ip_address = request.form.get("ip-address")
 
-    return render_template(
-        "pages/home.html",
-        ip_address=ip_address,
-        address=address,
-        time=time,
-        isp=isp,
-        latitude=latitude,
-        longitue=longitue,
-    )
+        data = home_page_controller.execute(ip_address)
+
+        ip_address = data["ip_address"]
+        address = data["address"]
+        time = data["time"]
+        isp = data["isp"]
+        latitude = data["latitude"]
+        longitue = data["longitue"]
+
+        return render_template(
+            "pages/home.html",
+            ip_address=ip_address,
+            address=address,
+            time=time,
+            isp=isp,
+            latitude=latitude,
+            longitue=longitue,
+        )
+    except Exception as exception:
+        flash(str(exception))
+        return redirect("/")
