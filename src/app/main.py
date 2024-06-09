90a4e63d689b30d4518695dc8c55f56ba96d3bf9
JohnPetros <joaopedro.nc@outlook.com>
2024-06-08 21:31:56 -0300

    ⚙️ config: init app

---
+++ src/app/main.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+from views import init_views
+
+
+def init_app():
+    app = Flask(
+        __name__, template_folder="../ui/templates", static_folder="../ui/static"
+    )
+
+    init_views(app)
+
+    return app
